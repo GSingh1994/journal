c44b0755275157cf3057299d4d191b75dba1f7d6
GSingh1994 <singh.gagandeep0034@gmail.com>
2021-04-06 04:29:16 -0500

    Added three components and node-sass

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <div className="form">
+      <form action="">
+        <input type="text" placeholder="Title" />
+        <textarea></textarea>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>My journal</h1>
+    </div>
+  );
+};
+
+export default Navbar;
